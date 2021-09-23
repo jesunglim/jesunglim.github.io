78d1b41da6973657cc63865613d8efa3be50a95b
jesung <jesunglimkorea@gmail.com>
2021-09-23 22:55:23 +0900

    blog engine

--- manager.py
+++ manager.py
@@ -1,19 +1,40 @@
 import os
+import json
 
 html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n    </head>\n	<body>"
 html_end = "\n        </div>\n    </body>\n</html>"
 html_header = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
+html_header_post_view = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
 
 
-html_list = ""
+
+html_list = "           "
 
 
 files = os.listdir("posts")
+files.reverse()
 for i in files:
-    print(i)
+    path = "posts/" + i
+    with open(path) as json_file:
+        json_data = json.load(json_file)
+        date = json_data["date"]
+        category = json_data["category"]
+        title = json_data["title"]
+        text = json_data["text"]
+        
+        link = i.split('.')[0]
+        html_list += "<a href= \"post_view/" + link + ".html\" style=\"text-decoration:none\">" + "<p>"+ date + " / " + category + "</p> <h1>" + title + "</h1> <p>" + text[0:50] + "</p> </a><br>"
 
-f = open("../index.html","wt")
-f.writelines(html_start + html_header + html_end)
+        html_post = "           <h1>" + title + "</h1> <p>" + date + " / category : " + category + "</p><hr><br><br> <p>" + text + "</p>"  
+        html_path = "post_view/" + link + ".html"
+        f = open(html_path,"wt")
+        f.writelines(html_start + html_header_post_view + html_post + html_end)
+        f.close()
+
+
+
+f = open("index.html","wt")
+f.writelines(html_start + html_header + html_list + html_end)
 f.close()
 
 
--- post.py
+++ post.py
@@ -2,33 +2,17 @@ import os
 import json
 import editer
 
-html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n    </head>\n	<body>"
-html_end = "\n        </div>\n    </body>\n</html>"
-html_header = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
-
-html_post_info = ""
-html_post = ""
-
-
-
-
 
 data = {
-    "date" :  editer.date,
-    "category" : editer.category,
-    "title" : editer.title,
-    "text" : editer.text,
+    "date" :  str(editer.date),
+    "category" : str(editer.category),
+    "title" : str(editer.title),
+    "text" : str(editer.text),
 }
 
+file_num = len(os.listdir("posts"))
+file_num = str(file_num + 1)
+path = "posts/" + file_num + ".json"
+with open(path, "w") as json_file:
+    json.dump(data, json_file, ensure_ascii=False)
 
-with open("student_file.json", "w") as json_file:
-
-    json.dump(data, json_file)
-
-
-
-
-path = "post_view/1.html"
-f = open(path,"wt")
-f.writelines(html_start + html_header + str + html_end)
-f.close()
