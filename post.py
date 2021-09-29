7f19641b6b94e318962851a5185f145ef7ec1435
jesung <jesunglimkorea@gmail.com>
2021-09-29 15:36:00 +0900

    layout work

--- post.py
+++ post.py
@@ -18,10 +18,11 @@ with open(path, "w") as json_file:
 
 
 # make html
-html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n    </head>\n	<body>"
-html_end = "\n        </div>\n    </body>\n</html>"
-html_header = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
-html_header_post_view = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
+html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n<link rel=\"stylesheet\" href=\"theme_index.css\">\n    </head>\n	<body>"
+html_start_page = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n<link rel=\"stylesheet\" href=\"../theme_page.css\">\n    </head>\n	<body>"
+html_end = "\n		</span>\n</div>\n\n    </body>\n</html>"
+html_header = "\n        <div class=\"outer\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n</div>\n<div class=\"inner\">\n	<span class=\"absolute\">"
+html_header_post_view = "\n        <div class=\"outer\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n</div>\n<div class=\"inner\">\n	"
 
 
 
@@ -43,12 +44,12 @@ for i in files:
         text_prev = text_prev[0:50]
         
         link = i.split('.')[0]
-        html_list += "<a href= \"post_view/" + link + ".html\" style=\"text-decoration:none\">" + "<p>"+ date + " / " + category + "</p> <h1>" + title + "</h1> <p>" + text_prev + "</p> </a><br>"
+        html_list += "\n<a href= \"post_view/" + link + ".html\" style=\"text-decoration:none\">" + "<p>"+ date + " / " + category + "</p> <h1>" + title + "</h1> <p>" + text_prev + "</p> </a><br>"
 
-        html_post = "           <h1>" + title + "</h1> <p>" + date + " / category : " + category + "</p><hr><br><br> <p>" + text + "</p>"  
+        html_post = "\n<div class=\"title\">\n    <h1>" + title + "</h1> <p>" + date + " / category : " + category + "</p>" + "\n</div>\n" +"<hr><br><br> <p>" + text + "</p>" + "\n<span class=\"absolute\">" 
         html_path = "post_view/" + link + ".html"
         f = open(html_path,"wt")
-        f.writelines(html_start + html_header_post_view + html_post + html_end)
+        f.writelines(html_start_page + html_header_post_view + html_post + html_end)
         f.close()
 
 
--- refresh.py
+++ refresh.py
@@ -1,10 +1,14 @@
 import os
 import json
+import editer
 
-html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n    </head>\n	<body>"
-html_end = "\n        </div>\n    </body>\n</html>"
-html_header = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
-html_header_post_view = "\n        <div style=\"text-align: center\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n"
+
+# make html
+html_start = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n<link rel=\"stylesheet\" href=\"theme_index.css\">\n    </head>\n	<body>"
+html_start_page = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <title>임재성 기술 블로그</title>\n<link rel=\"stylesheet\" href=\"../theme_page.css\">\n    </head>\n	<body>"
+html_end = "\n		</span>\n</div>\n\n    </body>\n</html>"
+html_header = "\n        <div class=\"outer\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n</div>\n<div class=\"inner\">\n	<span class=\"absolute\">"
+html_header_post_view = "\n        <div class=\"outer\">\n            <div class=\"head\">\n                <h1>임재성 기술 블로그</h1>\n                <img src=\"../images/bio-photo.jpg\" alt=\"\">\n                <p>email : jesunglimkorea@gmail.com</p>\n                <A href=\"https://github.com/jesunglim\"> GitHub</A>\n                <p>호서대학교 학부생입니다.</p>\n                <hr style=\"height:3px;border:none;color:#333;background-color:#333;\">\n                <br><br><br><br>\n            </div>\n</div>\n<div class=\"inner\">\n	"
 
 
 
@@ -21,17 +25,17 @@ for i in files:
         category = json_data["category"]
         title = json_data["title"]
         text = json_data["text"]
-
+        
         text_prev = str(text.split("<img")[0])
         text_prev = text_prev[0:50]
-
+        
         link = i.split('.')[0]
-        html_list += "<a href= \"post_view/" + link + ".html\" style=\"text-decoration:none\">" + "<p>"+ date + " / " + category + "</p> <h1>" + title + "</h1> <p>" + text_prev + "</p> </a><br>"
+        html_list += "\n<a href= \"post_view/" + link + ".html\" style=\"text-decoration:none\">" + "<p>"+ date + " / " + category + "</p> <h1>" + title + "</h1> <p>" + text_prev + "</p> </a><br>"
 
-        html_post = "           <h1>" + title + "</h1> <p>" + date + " / category : " + category + "</p><hr><br><br> <p>" + text + "</p>"  
+        html_post = "\n<div class=\"title\">\n    <h1>" + title + "</h1> <p>" + date + " / category : " + category + "</p>" + "\n</div>\n" +"<hr><br><br> <p>" + text + "</p>" + "\n<span class=\"absolute\">" 
         html_path = "post_view/" + link + ".html"
         f = open(html_path,"wt")
-        f.writelines(html_start + html_header_post_view + html_post + html_end)
+        f.writelines(html_start_page + html_header_post_view + html_post + html_end)
         f.close()
 
 
@@ -39,5 +43,3 @@ for i in files:
 f = open("index.html","wt")
 f.writelines(html_start + html_header + html_list + html_end)
 f.close()
-
-
